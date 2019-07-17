1d4888303f6915529b3524e1b98f2a7d9163ebf1
Jim Alexander <alexanderj2396@gmail.com>
2019-07-17 20:53:29 +1000

    height and icon padding

--- src/Firebase.js
+++ src/Firebase.js
@@ -5,10 +5,7 @@ const config = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASEURL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID
-};
+ };
 
 if (!firebase.apps.length) {
   firebase.initializeApp(config);
